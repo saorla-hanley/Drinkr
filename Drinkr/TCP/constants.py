eee85241e3b505f31080bf479959dd1203333243
jack-bosley <jackbosley@hotmail.com>
2021-03-14 12:08:40 +0000

    Starting again with Django Channels

--- Drinkr/TCP/constants.py
+++
@@ -1,8 +0,0 @@
-
-from enum import Enum
-
-class server_command_types(Enum):
-    Log_Rooms = 0
-    Log_Users = 1
-
-    Welcome = 100
--- Server/RTS-Server/Room.py
+++
@@ -1,6 +0,0 @@
-
-class Room(object):
-
-    def __init__(self, key):
-        self.room_key = key
-
